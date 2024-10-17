82aeedab8177e1afa56f3375ba719697cc5399e9
Matheus0082 <matheusvicente492@gmail.com>
2024-10-17 09:22:47 -0300

    'backend'

--- front/js/script.js
+++ front/js/script.js
@@ -20,3 +20,26 @@ async function obterFilmes() {
     }
 //     console.log(filmes)
  }
+ async function cadastrarFilme() {
+    const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
+    let tituloInput  = document.querySelector('#tituloInput')
+    let sinopseInput = document.querySelector('#sinopseInput')
+    let titulo = tituloInput.value 
+    let sinopse = sinopseInput.value
+    tituloInput.value = ""
+    sinopseInput.value = "" 
+    const filmes = (await axios.post (URLcompleta, {titulo, sinopse})).data
+    let tabela = document.querySelector('.filmes')
+    let corpoTabela = tabela.getElementsByTagName('tbody')[0]
+    corpoTabela.innerHTML = ""
+    for (let filme of filmes) {
+        //para cada filme, inserir uma linha
+        let linha = corpoTabela.insertRow(0)
+        //para cada linha, insere 2 células
+        let celulaTitulo = linha.insertCell(0)
+        let celulaSinopse = linha.insertCell(1)
+        //atribui o HTML para cada célula
+        celulaTitulo.innerHTML = filme.titulo
+        celulaSinopse.innerHTML = filme.sinopse
+    }
+ }
